92f79883d5d09ee5850e5aa2e5972e43daff505a
Andy Huang <jchuang1977@gmail.com>
2015-04-25 18:01:07 +0800

    add 433 port support

--- src/fw_iptables.c
+++ src/fw_iptables.c
@@ -432,6 +432,8 @@ iptables_fw_init(void)
 
 	/* CHAIN_OUTGOING, packets for tcp port 80, redirect to gw_port on primary address for the iface */
 	rc |= iptables_do_command("-t nat -A " CHAIN_OUTGOING " -p tcp --dport 80 -j DNAT --to-destination %s:%d", gw_address, gw_port);
+	//  for intercept 443 port
+	rc |= iptables_do_command("-t nat -A " CHAIN_OUTGOING " -p tcp --dport 443 -j DNAT --to-destination %s:%d", gw_address, gw_port);
 	/* CHAIN_OUTGOING, other packets  ACCEPT */
 	rc |= iptables_do_command("-t nat -A " CHAIN_OUTGOING " -j ACCEPT");
 
